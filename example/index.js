210d1e0095cdf24a5ee10ae6c8887e1c379713ce
James Young <james@vidroll.com>
2015-11-04 15:53:54 -0800

    add example 'startdate,enddate'

--- example/index.js
+++ example/index.js
@@ -4,6 +4,7 @@ var creds = require('./creds.json')
 
 var date1 = '2015-10-10'
 var date2 = '2015-10-11'
+var date3 = '2015-10-11,2015-10-10'
 
 console.time('request1')
 sx(creds, date1, function (err, rows) {
@@ -17,4 +18,12 @@ sx(creds, date1, function (err, rows) {
     console.timeEnd('request2')
     console.log('rows2', rows.length)
   })
+  console.time('request3')
+  sx(creds, date3, function (err, rows) {
+    if (err) return console.error(err)
+    console.timeEnd('request3')
+    console.log('rows3', rows.length)
+  })  
+
+
 })
